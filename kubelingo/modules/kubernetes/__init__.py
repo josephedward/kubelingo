8f18a36d1a8156946f5e7190e8a5c224fd41b6fc
josephedward <josephedwardwork@gmail.com>
2025-07-16 14:59:26 -0400

    chore: Clean up redundant imports

--- kubelingo/modules/kubernetes/__init__.py
+++ kubelingo/modules/kubernetes/__init__.py
@@ -2,6 +2,5 @@
 Kubernetes study session plugin package.
 """
 from .session import NewSession as KubernetesSession
+
 __all__ = ['KubernetesSession']
-from .session import KubernetesSession
-from .session import NewSession as KubernetesSession
