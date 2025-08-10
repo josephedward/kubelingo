28b111b4c5524dba91d5c1bb88fad179d81d190b
josephedward <josephedwardwork@gmail.com>
2025-08-10 00:21:45 -0400

    Create config.py

---
+++ kubelingo/config.py
@@ -0,0 +1,7 @@
+"""
+Backward-compatibility module to expose top-level configurations.
+"""
+from kubelingo.utils.config import DEFAULT_DATA_FILE, ENABLED_QUIZZES
+
+# Export default public API
+__all__ = ['DEFAULT_DATA_FILE', 'ENABLED_QUIZZES']
