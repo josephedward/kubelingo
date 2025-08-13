042c22d7f35b0636e97b9c84486ee39f501d915d
josephedward <josephedwardwork@gmail.com>
2025-08-13 12:37:06 -0400

    Delete config.py

--- kubelingo/config.py
+++
@@ -1,8 +0,0 @@
-"""
-Backward-compatibility module to expose top-level configurations.
-"""
-# This file is retained for backward compatibility but is now empty
-# as quiz configurations are loaded directly from the database.
-
-# Export default public API
-__all__ = []
