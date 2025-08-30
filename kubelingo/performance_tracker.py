62a376b60dc46f2d23200879460ed70fe2afdab4
Test User <josephedwardwork@gmail.com>
2025-08-30 12:52:23 -0400

    Create performance_tracker.py

---
+++ kubelingo/performance_tracker.py
@@ -0,0 +1,19 @@
+import yaml
+from colorama import Fore, Style
+from kubelingo.utils import USER_DATA_DIR, PERFORMANCE_FILE, ensure_user_data_dir
+
+_performance_data_changed = False # Global flag to track changes to performance data
+
+def save_performance_data(data):
+    """Saves performance data."""
+    global _performance_data_changed
+    if not _performance_data_changed:
+        return
+
+    ensure_user_data_dir()
+    try:
+        with open(PERFORMANCE_FILE, 'w') as f:
+            yaml.dump(data, f)
+        _performance_data_changed = False
+    except Exception as e:
+        print(f"{Fore.RED}Error saving performance data to '{PERFORMANCE_FILE}': {e}{Style.RESET_ALL}")
