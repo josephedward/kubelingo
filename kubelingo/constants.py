0fab92ddb710aabe5af471c6213de65b56e93c31
Test User <josephedwardwork@gmail.com>
2025-09-07 14:53:41 -0400

    Create constants.py

---
+++ kubelingo/constants.py
@@ -0,0 +1,9 @@
+SUBJECT_MATTERS = [
+    'api_discovery_docs', 'app_configuration', 'commands_args_env', 'configmap', 'core_workloads',
+    'deployment', 'helm_basics', 'image_registry_use', 'imperative_vs_declarative', 'ingress_http_routing',
+    'jobs_cronjobs', 'kubectl_common_operations', 'kubectl_operations', 'labels_annotations_selectors',
+    'linux_commands_syntax', 'logging', 'namespaces_contexts', 'networking_utilities',
+    'observability_troubleshooting', 'persistence', 'pod_design_patterns', 'probes_health', 'pvc',
+    'resource_management', 'resource_reference', 'scheduling_hints', 'secrets', 'security_basics',
+    'service_accounts_in_apps', 'services', 'rbac', 'monitoring', 'troubleshooting'
+]
